00b223afc13584914c79975a822800edf425eea4
tzing <tzingshih@gmail.com>
2025-11-28 23:51:31 +0800

    fix: TWQRP format

--- src/utils/twqrUrl.ts
+++ src/utils/twqrUrl.ts
@@ -1,17 +1,3 @@
-function getLocalTimestamp(): string {
-  const now = new Date();
-  const pad = (n: number) => n.toString().padStart(2, "0");
-
-  return (
-    now.getFullYear().toString() +
-    pad(now.getMonth() + 1) +
-    pad(now.getDate()) +
-    pad(now.getHours()) +
-    pad(now.getMinutes()) +
-    pad(now.getSeconds())
-  );
-}
-
 export interface TWQRPUrlOptions {
   bankCode: string
   accountId: string
@@ -19,17 +5,18 @@ export interface TWQRPUrlOptions {
 }
 
 export const generateTWQRPUrl = ({ bankCode, accountId, amount }: TWQRPUrlOptions): string => {
-  const url = new URL(`TWQRP://個人轉帳/158/02/V1`)
+  // TWQRP://個人轉帳/158/02/V1
+  const baseUrl = `TWQRP://xn--gmqw5ax42ad01c/158/02/V1`
+  const params = new URLSearchParams()
 
-  url.searchParams.append('D5', bankCode)
+  params.append('D5', bankCode)
   // account ID needs to be padded to 16 digits
-  url.searchParams.append('D6', accountId.padStart(16, '0'))
-  url.searchParams.append('D97', getLocalTimestamp())
+  params.append('D6', accountId.padStart(16, '0'))
 
   if (amount) {
-    url.searchParams.append('D10', '901')
-    url.searchParams.append('D1', Math.floor(amount * 100).toString())
+    params.append('D10', '901')
+    params.append('D1', Math.floor(amount * 100).toString())
   }
 
-  return url.toString()
+  return `${baseUrl}?${params.toString()}`
 }
